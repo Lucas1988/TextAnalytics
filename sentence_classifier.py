ee3f9a959b9ef016a4002cb4473e7dad8bcdb654
LucasVergeest <lucas.vergeest@futurefacts.nl>
2020-04-10 10:42:29 +0200

    updates

--- sentence_classifier.py
+++ sentence_classifier.py
@@ -7,7 +7,7 @@ from sklearn import svm
 from sklearn.neural_network import MLPClassifier
 
 model = SentenceTransformer('distiluse-base-multilingual-cased')
-sentences = pd.read_excel('sentences_supervised_2.xlsx', sheet_name='sheet')
+sentences = pd.read_excel('sentences_supervised.xlsx', sheet_name='Sheet1')
 sentences_set = sentences['Sentences']
 sentences_set = [re.sub('[\n\r]', ' ', sentence) for sentence in sentences_set]	
 
@@ -16,6 +16,7 @@ print('Creating embeddings...')
 sentence_embeddings = model.encode(sentences_set)
 labels = sentences['Labels']
 
+# Split data in train and test set
 embeddings_train, embeddings_test, y_train, y_test = train_test_split(
 	sentence_embeddings, labels, test_size=0.2, random_state=1000)
 
